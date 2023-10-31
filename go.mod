cb83d49d427b9b0e1da84fdacdea5a7c9be94ab8
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-31 12:15:12 +0100

    feat: setup .env validation

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Laurin-Notemann/tennis-analysis
 
 go 1.20
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	_ "github.com/lib/pq"
+
+	"github.com/Laurin-Notemann/tennis-analysis/config"
+)
+
 func main() {
+	// Parse Config
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("error loading .env file: %v\n", err)
+	}
+
+	var cfg config.Config
+	err = envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatalf("can't parse config: %v", err)
+	}
+
+  log.Printf("env: %v\n", cfg.DB.Url)
+
 }
