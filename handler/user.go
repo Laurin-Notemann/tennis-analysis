3a48c58dd7d4adac5bbadb8749d87c31d9d67fc4
Laurin-Notemann <laurin.notemann@gmail.com>
2023-11-03 18:56:39 +0100

    feat: implement GetAllUsers and GetUserById handler methods

--- handler/user.go
+++ handler/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/Laurin-Notemann/tennis-analysis/db"
+	"github.com/gofrs/uuid"
 )
 
 type UserHandler struct {
@@ -12,15 +13,31 @@ type UserHandler struct {
 
 func NewUserHandler(DBTX *db.Queries) *UserHandler {
 	return &UserHandler{
-    DB: DBTX,
+		DB: DBTX,
 	}
 }
 
 func (u *UserHandler) CreateUser(ctx context.Context, newUser db.CreateUserParams) (db.User, error) {
-	row, err := u.DB.CreateUser(ctx, newUser)
+	user, err := u.DB.CreateUser(ctx, newUser)
 	if err != nil {
 		return db.User{}, err
 	}
 
-	return row, nil
+	return user, nil
+}
+
+func (u *UserHandler) GetAllUsers(ctx context.Context) ([]db.User, error) {
+	users, err := u.DB.GetAllUsers(ctx)
+	if err != nil {
+		return []db.User{}, err
+	}
+	return users, nil
+}
+
+func (u *UserHandler) GetUserById(ctx context.Context, id uuid.UUID) (db.GetUserByIdRow, error) {
+	user, err := u.DB.GetUserById(ctx, id)
+	if err != nil {
+		return db.GetUserByIdRow{}, err
+	}
+	return user, nil
 }
