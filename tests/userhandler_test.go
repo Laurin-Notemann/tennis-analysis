40778fbf4854a307d957b520a308fa8c67244128
Laurin-Notemann <laurin.notemann@gmail.com>
2023-11-04 15:49:31 +0100

    test: add tests for getUserById and getAllUsers

--- tests/userhandler_test.go
+++ tests/userhandler_test.go
@@ -8,32 +8,56 @@ import (
 	"github.com/Laurin-Notemann/tennis-analysis/handler"
 )
 
-func TestCreateUser(t *testing.T) {
+func TestUserHandler(t *testing.T) {
 	newUser := db.CreateUserParams{
 		Username:     "laurin",
 		Email:        "laurin@test.de",
 		PasswordHash: "Test",
 	}
 	dbMock := NewDBQueriesMock()
-
 	userHandler := handler.UserHandler{
 		DB: dbMock,
 	}
 
-	user, err := userHandler.CreateUser(context.Background(), newUser)
-	if err != nil {
-		t.Fatalf("userHanlder.CreateUser(%+v) = nil, %v, want match for correct User", newUser, err)
-	}
 	correctUser := db.User{
-		ID:           user.ID,
 		Username:     "laurin",
 		Email:        "laurin@test.de",
 		PasswordHash: "Test",
-		CreatedAt:    user.CreatedAt,
-		UpdatedAt:    user.UpdatedAt,
 	}
 
-	if user != correctUser {
-		t.Fatalf("userHanlder.CreateUser(%+v) = %+v, nil, want match for correct %+v, nil", newUser, user, correctUser)
-	}
+	t.Run("CreateUser", func(t *testing.T) {
+		user, err := userHandler.CreateUser(context.Background(), newUser)
+		if err != nil {
+			t.Fatalf("userHanlder.CreateUser(%+v) = nil, %v, want match for correct User", newUser, err)
+		}
+
+		correctUser.ID = user.ID
+		correctUser.CreatedAt = user.CreatedAt
+		correctUser.UpdatedAt = user.UpdatedAt
+
+		if user != correctUser {
+			t.Fatalf("userHanlder.CreateUser(%+v) = %+v, nil, want match for correct %+v, nil", newUser, user, correctUser)
+		}
+	})
+
+	t.Run("GetAllUsers", func(t *testing.T) {
+		users, err := userHandler.GetAllUsers(context.Background())
+		if err != nil {
+			t.Fatalf("userHandler.GetAllUsers(), err when trying to get users: %v", err)
+		}
+		if len(users) != 1 {
+			t.Fatalf("userHandler.GetAllUsers(), want list of users with one entry, got %#v", users)
+		}
+	})
+
+	t.Run("GetUserById", func(t *testing.T) {
+		user, err := userHandler.GetUserById(context.Background(), correctUser.ID)
+		if err != nil {
+			t.Fatalf("userhandler.GetUserById(), err when tryong to get user by id: %v", err)
+		}
+		if user.Email != correctUser.Email && user.ID != correctUser.ID && correctUser.Username != user.Username {
+			t.Fatalf("userHandler.GetAllUsers(), want %#v, got %#v", correctUser, user)
+		}
+	})
+
 }
