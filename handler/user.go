f71039fc124a3983d779f50fcab15a5ab99e09f5
Laurin-Notemann <laurin.notemann@gmail.com>
2023-11-04 19:04:49 +0100

    feat: implement delete update and get methods for user handler

--- handler/user.go
+++ handler/user.go
@@ -34,10 +34,42 @@ func (u *UserHandler) GetAllUsers(ctx context.Context) ([]db.User, error) {
 	return users, nil
 }
 
-func (u *UserHandler) GetUserById(ctx context.Context, id uuid.UUID) (db.GetUserByIdRow, error) {
+func (u *UserHandler) GetUserById(ctx context.Context, id uuid.UUID) (db.User, error) {
 	user, err := u.DB.GetUserById(ctx, id)
 	if err != nil {
-		return db.GetUserByIdRow{}, err
+		return db.User{}, err
+	}
+	return user, nil
+}
+
+func (u *UserHandler) GetUserByEmail(ctx context.Context, email string) (db.User, error) {
+	user, err := u.DB.GetUserByEmail(ctx, email)
+	if err != nil {
+		return db.User{}, err
+	}
+	return user, nil
+}
+
+func (u *UserHandler) GetUserByUsername(ctx context.Context, username string) (db.User, error) {
+	user, err := u.DB.GetUserByUsername(ctx, username)
+	if err != nil {
+		return db.User{}, err
+	}
+	return user, nil
+}
+
+func (u *UserHandler) UpdateUserById(ctx context.Context, args db.UpdateUserByIdParams) (db.User, error) {
+	user, err := u.DB.UpdateUserById(ctx, args)
+	if err != nil {
+		return db.User{}, err
+	}
+	return user, nil
+}
+
+func (u *UserHandler) DeleteUserById(ctx context.Context, id uuid.UUID) (db.User, error) {
+	user, err := u.DB.DeleteUserById(ctx, id)
+	if err != nil {
+		return db.User{}, err
 	}
 	return user, nil
 }
