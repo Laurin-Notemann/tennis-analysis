775ee474cda4122f0d0c6ac6254e228f3cd78d18
Laurin-Notemann <laurin.notemann@gmail.com>
2023-11-13 16:18:03 +0100

    fix: add mock functions to satisfy interface (empty for now)

---
+++ utils/player-queries.mock.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"context"
+
+	"github.com/Laurin-Notemann/tennis-analysis/db"
+	"github.com/google/uuid"
+)
+
+func (d *DBQueriesMock) GetPlayerById(ctx context.Context, id uuid.UUID) (db.Player, error) {
+	return db.Player{}, nil
+}
