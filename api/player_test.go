56f4a2e469a857cd4df7ca89cb6d829203101a4e
Laurin-Notemann <laurin.notemann@gmail.com>
2023-11-14 11:47:02 +0100

    test(TDD): create failing test for missing GetAllPlayersById from players route

--- api/player_test.go
+++ api/player_test.go
@@ -20,6 +20,13 @@ type TestPlayerInput struct {
 	input db.CreateNewTeamWithOnePlayerParams
 }
 
+type TestGetAllPlayer struct {
+	name           string
+	error          TestError
+	input          []db.CreateNewTeamWithOnePlayerParams
+	expectedLength int
+}
+
 var testDb = utils.DbQueriesTest()
 var playerHandler = handler.NewPlayerHandler(testDb)
 var teamHandler = handler.NewTeamHandler(testDb)
@@ -128,33 +135,116 @@ func TestCreatePlayer(t *testing.T) {
 		},
 	}
 	for _, data := range testCreatePlayerInput {
-		input, err := json.Marshal(data.input)
-		assert.NoError(t, err, "Problem with encoding the user")
+		t.Run("create player "+data.name, func(t *testing.T) {
+			input, err := json.Marshal(data.input)
+			assert.NoError(t, err, "Problem with encoding the user")
 
-		err, rec, _ := DummyRequest(
-			t,
-			e,
-			http.MethodPost,
-			"/api/players",
-			string(input),
-			playRouter.CreatePlayer,
-		)
-		if data.error.IsError {
-      if assert.Error(t, err) {
-        assert.Equal(t, data.error.ExpectedError, err)
-      }
-		} else {
-			if assert.NoError(t, err, "Error with CreatePlayer route") {
-				newPlayer := new(db.Player)
-				err := json.Unmarshal(rec.Body.Bytes(), newPlayer)
-				assert.NoError(t, err, "Couldn't decode Player")
+			err, rec, _ := DummyRequest(
+				t,
+				e,
+				http.MethodPost,
+				"/api/players",
+				string(input),
+				playRouter.CreatePlayer,
+			)
+			if data.error.IsError {
+				if assert.Error(t, err) {
+					assert.Equal(t, data.error.ExpectedError, err)
+				}
+			} else {
+				if assert.NoError(t, err, "Error with CreatePlayer route") {
+					newPlayer := new(db.Player)
+					err := json.Unmarshal(rec.Body.Bytes(), newPlayer)
+					assert.NoError(t, err, "Couldn't decode Player")
 
-				assert.Equal(t, data.input.FirstName, newPlayer.FirstName)
-				assert.Equal(t, data.input.LastName, newPlayer.LastName)
+					assert.Equal(t, data.input.FirstName, newPlayer.FirstName)
+					assert.Equal(t, data.input.LastName, newPlayer.LastName)
+				}
 			}
-		}
+		})
 	}
 	_, err := userHandler.DeleteUserById(context.Background(), userId)
 	assert.NoError(t, err)
 }
 
+func TestGetAllPlayersByUserId(t *testing.T) {
+	e := echo.New()
+
+	user := DummyUser(t, e)
+	userId := user.ID
+
+	testDataInput := []TestGetAllPlayer{
+		{
+			name: "success",
+			error: TestError{
+				IsError:       false,
+				ExpectedError: nil,
+			},
+			input: []db.CreateNewTeamWithOnePlayerParams{
+				{
+					FirstName: "Laurin",
+					LastName:  "Notemann",
+					Name: sql.NullString{
+						String: "",
+						Valid:  false,
+					},
+					UserID: userId,
+				},
+				{
+					FirstName: "Max",
+					LastName:  "Mustermann",
+					Name: sql.NullString{
+						String: "",
+						Valid:  false,
+					},
+					UserID: userId,
+				},
+			},
+			expectedLength: 2,
+		},
+	}
+
+	for _, data := range testDataInput {
+		t.Run("create player "+data.name, func(t *testing.T) {
+			err, rec, _ := DummyRequest(
+				t,
+				e,
+				http.MethodPost,
+				"/api/players",
+				string(""),
+				playRouter.GetAllPlayerById,
+			)
+			if data.error.IsError {
+				if assert.Error(t, err) {
+					assert.Equal(t, data.error.ExpectedError, err)
+				}
+			} else {
+				if assert.NoError(t, err, "Error with CreatePlayer route") {
+					allPlayers := new([]db.Player)
+					err := json.Unmarshal(rec.Body.Bytes(), allPlayers)
+					assert.NoError(t, err, "Couldn't decode Player")
+
+					assert.Equal(t, data.expectedLength, len(*allPlayers))
+				}
+			}
+		})
+	}
+}
+
+func addMultiplePlayers(t *testing.T, e *echo.Echo, input []db.CreateNewTeamWithOnePlayerParams) {
+	for _, data := range input {
+		encodedData, err := json.Marshal(data)
+		assert.NoError(t, err, "Problem with encoding the user")
+
+		err, _, _ = DummyRequest(
+			t,
+			e,
+			http.MethodPost,
+			"/api/players",
+			string(encodedData),
+			playRouter.CreatePlayer,
+		)
+		assert.NoError(t, err, "Problem with adding new player")
+	}
+
+}
